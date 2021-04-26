9c72c7b29ca5e3329d176b6c762c32dfab35dff8
fahadalsaedi <alsaedifahad58@gmail.com>
2021-04-26 07:14:31 +0300

    create first Post

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Post(models.Model):
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     post = models.TextField(max_length=1000)
-    date_posted = models.DateTimeField(blank=True, null=True)
+    date_posted = models.DateTimeField(blank=True)
    
-
-
-
+    def __str__(self):
+       return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-
-posts =[
-    {
-        'author': 'fahad',
-        'title': 'blog post 1',
-        'date_posted': 'Agust 27, 2020'
-    },
-      {
-        'author': 'adel',
-        'title': 'blog post 2',
-        'date_posted': 'may 23, 2020'
-    }
-
-]
+from .models import Post
 
 
 def home(request):
     context ={
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
