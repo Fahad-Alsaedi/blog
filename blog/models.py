37fb1397a6ffd952fb296914c302033b5e92c970
fahadalsaedi <alsaedifahad58@gmail.com>
2021-04-26 03:57:00 +0300

    configer and create tables with poostgrSQL

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    post = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(blank=True, null=True)
+   
+
+
+
